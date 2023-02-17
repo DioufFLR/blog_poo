536b94ca60c503b1daafc3e64f2685e1af5211eb
DioufFLR <geoffreyfleur@gmail.com>
2023-02-17 11:33:41 +0100

    refact db include and render

---
+++ libraries/database.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @return PDO
+ */
+function getPdo(): PDO
+{
+    $pdo = new PDO('mysql:host=localhost;dbname=blogpoo;charset=utf8', 'user', '2704', [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+    return $pdo;
+}
