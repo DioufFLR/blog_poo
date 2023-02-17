90ce884bc7cdd1c735389329eebc2fbf9cb064fd
DioufFLR <geoffreyfleur@gmail.com>
2023-02-17 12:14:26 +0100

    refact files php comment and article

--- libraries/database.php
+++ libraries/database.php
@@ -11,3 +11,39 @@ function getPdo(): PDO
     ]);
     return $pdo;
 }
+
+/**
+ * Retourne la liste des articles classés par date de création
+ *
+ * @return array
+ */
+function findAllArticles(): array
+{
+    $pdo = getPdo();
+    $resultats = $pdo->query('SELECT * FROM articles ORDER BY created_at DESC');
+    $articles = $resultats->fetchAll();
+
+    return $articles;
+}
+
+function findArticle(int $id): array
+{
+    $pdo = getPdo();
+
+    $query = $pdo->prepare("SELECT * FROM articles WHERE id = :article_id");
+    $query->execute(['article_id' => $id]);
+    $article = $query->fetch();
+
+    return $article;
+}
+
+function findAllComments(int $article_id): array
+{
+    $pdo = getPdo();
+
+    $query = $pdo->prepare("SELECT * FROM comments WHERE article_id = :article_id");
+    $query->execute(['article_id' => $article_id]);
+    $commentaires = $query->fetchAll();
+
+    return $commentaires;
+}
