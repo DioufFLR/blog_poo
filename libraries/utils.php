3b5011814e9598992b3d82171f912f398807fa6c
DioufFLR <geoffreyfleur@gmail.com>
2023-02-19 12:51:05 +0100

    refact finish

--- libraries/utils.php
+++ libraries/utils.php
@@ -4,17 +4,15 @@
 function render(string $path, array $variables = [])
 {
     extract($variables);
-
     ob_start();
     require('templates/' . $path . '.html.php');
     $pageContent = ob_get_clean();
-
     require('templates/layout.html.php');
 }
 
 // redirect('index.php')
 function redirect(string $url): void
 {
-    header("Location: index.php");
+    header("Location: $url");
     exit();
 }
